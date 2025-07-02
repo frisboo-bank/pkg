f23cb855430b0f29b9301d3b123a929875dfb37e
Jonathan <jonathan.houze@gmail.com>
2025-07-02 15:40:06 +0800

    feat: avoid module to register 2 times the same sub-module

--- container/module.go
+++ container/module.go
@@ -1,5 +1,7 @@
 package container
 
+// Module represents a dependency-injectable module in the container system.
+// Each module can have submodules, providers, decorators, invokes, and hooks.
 type Module interface {
 	Dependency
 	GetName() string
@@ -31,30 +33,43 @@ func NewModule(name string, options ...Dependency) Module {
 	return m
 }
 
-func (m *module) apply(mp *module) {
-	mp.modules = append(mp.modules, m)
+// apply adds the module as a submodule to the target module pointer.
+func (m *module) apply(targetModule *module) {
+	for _, mod := range targetModule.modules {
+		if mod == m {
+			return
+		}
+	}
+
+	targetModule.modules = append(targetModule.modules, m)
 }
 
-func (m *module) GetDecorators() []Decorator {
-	return m.decorators
-}
-
-func (m *module) GetHooks() []HookStarter {
-	return m.hooks
-}
-
-func (m *module) GetInvokes() []Invoker {
-	return m.invokers
-}
-
-func (m *module) GetModules() []Module {
-	return m.modules
-}
-
+// returns the name of the module.
 func (m *module) GetName() string {
 	return m.name
 }
 
+// returns the submodules registered in the module.
+func (m *module) GetModules() []Module {
+	return m.modules
+}
+
+// returns the providers registered in the module.
 func (m *module) GetProviders() []Provider {
 	return m.providers
+}
+
+// returns the hooks registered in the module.
+func (m *module) GetHooks() []HookStarter {
+	return m.hooks
+}
+
+// returns the decorators registered in the module.
+func (m *module) GetDecorators() []Decorator {
+	return m.decorators
+}
+
+// returns the invokers registered in the module.
+func (m *module) GetInvokes() []Invoker {
+	return m.invokers
 }
